f6e205eb706b37e58346d3d96eaec25f90d2f80d
lewis <lewis.takavarasha@gmail.com>
2025-04-12 02:17:44 +0100

    added initial backend setup

---
+++ taskit-be/taskit-be/src/main/java/com/dts/taskit_be/TaskitBeApplication.java
@@ -0,0 +1,13 @@
+package com.dts.taskit_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskitBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskitBeApplication.class, args);
+	}
+
+}
---
+++ taskit-be/taskit-be/src/test/java/com/dts/taskit_be/TaskitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dts.taskit_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskitBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
